c299fcfd2b28c6f7fa811051a42920df7695583b
leex5818-sketch <leex5818@gmail.com>
2026-05-12 11:26:45 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+// 항상 최신 파일 사용 (캐시 비활성화)
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
